69b59aac2e3f9e9399307f008ef533222b878c0f
AlexKharenko <alexey16052002@gmail.com>
2021-10-19 20:03:05 +0300

    Styled about me page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,43 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <div class="about-me-title">
+      <h1>Oleksii Kharenko</h1>
+      <h3>Front-end developer</h3>
+    </div>
+    <div class="about-me-text-block">
+      <p class="about-me-paragraph">
+        Something about me. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Nulla quasi eius dicta, aspernatur ipsam assumenda, magnam,
+        debitis esse nobis nisi nesciunt quas enim. Enim vitae nemo minus!
+        Omnis, dolorem laboriosam!
+      </p>
+    </div>
   </div>
 </template>
+
+<style lang="scss">
+.about {
+  padding: 20px 80px;
+  .about-me-title {
+    text-align: center;
+    margin-bottom: 30px;
+  }
+  .about-me-text-block {
+    .about-me-paragraph {
+      text-indent: 45px;
+    }
+  }
+}
+.dark {
+  .about {
+    .about-me-title {
+      color: rgb(224, 222, 222);
+    }
+    .about-me-text-block {
+      .about-me-paragraph {
+        color: rgb(175, 172, 172);
+      }
+    }
+  }
+}
+</style>
