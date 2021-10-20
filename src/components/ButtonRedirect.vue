ff506bca5b1d1874f1d0a90416bcee2a6eaf6854
AlexKharenko <alexey16052002@gmail.com>
2021-10-20 22:59:57 +0300

    Creating component 'button for redirecting'

---
+++ src/components/ButtonRedirect.vue
@@ -0,0 +1,34 @@
+<template>
+  <router-link class="btn-link" :to="target_page">
+    <div class="btn-redirect">
+      <p class="btn-text">{{ btn_text || "More" }}</p>
+    </div>
+  </router-link>
+</template>
+
+<script>
+export default {
+  props: ["btn_text", "target_page"],
+};
+</script>
+
+<style lang="scss">
+.btn-link {
+  text-decoration: none;
+  color: var(--btn-first-color);
+  .btn-redirect {
+    width: 100px;
+    height: 40px;
+    text-align: center;
+    background: var(--btn-first-bg);
+    .btn-text {
+      position: relative;
+      top: 50%;
+      transform: translateY(-50%);
+    }
+  }
+  .btn-redirect:hover {
+    background: var(--btn-first-hover-bg);
+  }
+}
+</style>
