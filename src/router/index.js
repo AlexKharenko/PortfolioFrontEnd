75c323478a79f9d61760db186046f50641d065d8
AlexKharenko <alexey16052002@gmail.com>
2021-10-22 22:15:29 +0300

    git add new route for work details

--- src/router/index.js
+++ src/router/index.js
@@ -28,6 +28,16 @@ const routes = [
     },
   },
   {
+    path: "/works/:id",
+    name: "WorkDetails",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: function () {
+      return import(/* webpackChunkName: "about" */ "../views/WorkDetails.vue");
+    },
+  },
+  {
     path: "/contacts",
     name: "Contacts",
     // route level code-splitting
