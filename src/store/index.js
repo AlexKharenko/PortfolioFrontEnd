6b04ccf69d3fab408c81948a029a4737b79767eb
AlexKharenko <alexey16052002@gmail.com>
2021-11-09 19:14:27 +0200

    add the log_in module

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,10 @@
 import { createStore } from "vuex";
 import dark_mode from "./modules/dark_mode";
+import log_in from "./modules/log_in";
 
 export default createStore({
   modules: {
     dark_mode,
+    log_in,
   },
 });
