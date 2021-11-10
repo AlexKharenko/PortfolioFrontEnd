c10b1c72f16faa66d7f72c8b928aae66a25e9060
AlexKharenko <alexey16052002@gmail.com>
2021-11-10 23:07:23 +0200

    Admin panel

---
+++ src/views/Admin.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="admin-page">
+    <div class="redirect-btn-block">
+      <ButtonRedirect :btn_text="'Create Work'" target_page="/admin/add/work" />
+      <ButtonRedirect
+        :btn_text="'Add Language'"
+        target_page="/admin/add/lang"
+      />
+    </div>
+    <div class="admin-works">
+      <WorkItemAdmin
+        class="margin-top-10"
+        v-for="work in works"
+        :key="work.id"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import ButtonRedirect from "@/components/ButtonRedirect.vue";
+import WorkItemAdmin from "@/components/WorkItemAdmin.vue";
+
+export default {
+  name: "Admin",
+  components: {
+    ButtonRedirect,
+    WorkItemAdmin,
+  },
+  data() {
+    return {
+      works: [{ id: 1 }, { id: 2 }],
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.admin-page {
+  .redirect-btn-block {
+    grid-gap: 20px;
+    padding: 40px 0;
+    display: flex;
+    justify-content: center;
+  }
+  .admin-works {
+    padding: 0 40px;
+  }
+}
+</style>
