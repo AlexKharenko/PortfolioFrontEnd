0dc11c5395ab65f00b3b3b80168654886b21e40c
AlexKharenko <alexey16052002@gmail.com>
2021-11-10 23:04:14 +0200

    made multifunctional select

---
+++ src/components/CustomSelect.vue
@@ -0,0 +1,32 @@
+<template>
+  <select v-model="result">
+    <option disabled value="">Select {{ add_option_name }}</option>
+    <option v-for="item in items" :key="item.id" :value="item.id">
+      {{ item[obj_key] }}
+    </option>
+  </select>
+</template>
+
+<script>
+export default {
+  name: "CustomSelect",
+  props: ["items", "obj_key", "add_option_name"],
+  data() {
+    return {
+      result: "",
+    };
+  },
+  methods: {
+    emitResult() {
+      this.$emit("SelectResult", this.result);
+    },
+  },
+  watch: {
+    result() {
+      this.emitResult();
+    },
+  },
+};
+</script>
+
+<style></style>
