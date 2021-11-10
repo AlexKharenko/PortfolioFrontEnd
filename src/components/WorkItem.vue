45b59fe66e92556aaa7fba07f559f831fbd7a35c
AlexKharenko <alexey16052002@gmail.com>
2021-11-10 23:05:13 +0200

    fixed styling issue

--- src/components/WorkItem.vue
+++ src/components/WorkItem.vue
@@ -41,7 +41,7 @@ export default {
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .work-block {
   border-bottom: 1px solid var(--additional-dark-mode-color);
   padding-bottom: 20px;
