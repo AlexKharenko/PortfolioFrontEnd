3dc2e0dad1f83077e82469f04d5a5e741b30404c
AlexKharenko <alexey16052002@gmail.com>
2021-11-14 01:41:16 +0200

    add localstorage handling and fixed errors

--- src/i18n.js
+++ src/i18n.js
@@ -7,15 +7,17 @@ function loadLocaleMessages() {
     const matched = key.match(/([A-Za-z0-9-_]+)\./i);
     if (matched && matched.length > 1) {
       const locale = matched[1];
-      messages[locale] = locales(key).default;
+      messages[locale] = locales(key);
     }
   });
   return messages;
 }
 
 export default createI18n({
-  legacy: false,
-  locale: process.env.VUE_APP_I18N_LOCALE || "en",
+  locale:
+    JSON.parse(localStorage.getItem("language")) ||
+    process.env.VUE_APP_I18N_LOCALE ||
+    "en",
   fallbackLocale: process.env.VUE_APP_I18N_FALLBACK_LOCALE || "en",
   messages: loadLocaleMessages(),
 });
