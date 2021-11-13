109720380573238f69d96a3996495567f06e75f6
AlexKharenko <alexey16052002@gmail.com>
2021-11-13 23:13:35 +0200

    Message functionality

---
+++ src/store/modules/contact.js
@@ -0,0 +1,30 @@
+// import router from "@/router";
+
+const actions = {
+  // eslint-disable-next-line no-unused-vars
+  async sendEmail({ commit }, data) {
+    let response = await fetch(
+      `${process.env.VUE_APP_SERVER}/profile/message`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      }
+    );
+    if (response.status == 200) {
+      response = await response.json();
+      return response;
+    } else {
+      if (response.status == 429 || response.status == 505) {
+        response = await response.json();
+        return response;
+      }
+    }
+  },
+};
+
+export default {
+  actions,
+};
