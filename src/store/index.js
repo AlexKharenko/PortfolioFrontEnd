55ccdb89243387f9b4029ed2b55043b48dc1a81f
AlexKharenko <alexey16052002@gmail.com>
2021-11-13 23:14:37 +0200

    Connected contacts to vuex store

--- src/store/index.js
+++ src/store/index.js
@@ -2,11 +2,13 @@ import { createStore } from "vuex";
 import dark_mode from "./modules/dark_mode";
 import log_in from "./modules/log_in";
 import works from "./modules/works";
+import contact from "./modules/contact";
 
 export default createStore({
   modules: {
     dark_mode,
     log_in,
     works,
+    contact,
   },
 });
