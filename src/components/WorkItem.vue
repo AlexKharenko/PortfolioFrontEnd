8bc19f6f35d6ebabdee3e26c4d5df310f4430cf3
AlexKharenko <alexey16052002@gmail.com>
2021-11-13 23:06:55 +0200

    Connected to server and add responsive styling

--- src/components/WorkItem.vue
+++ src/components/WorkItem.vue
@@ -1,26 +1,26 @@
 <template>
-  <div class="work-block">
-    <div class="work-img">
-      <img src="../assets/Screenshot.png" alt="Work Image" />
-    </div>
-    <div class="work-data-block">
-      <div class="work-text-block">
-        <h3 class="work-title">Work</h3>
-        <p class="about-work-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos,
-          tenetur. Nemo beatae maxime aliquam mollitia incidunt sit at tempore
-          doloribus, dolore accusamus delectus impedit nisi iste quod totam
-          nobis aliquid. Cum amet corporis ipsum natus aut, obcaecati molestias
-          ea. Nisi quas expedita accusamus quod minima, tempore natus molestiae
-          excepturi. Tempore repellat beatae eos rem harum illo quia, blanditiis
-          expedita corrupti. Tenetur blanditiis quod assumenda aperiam
-          doloremque nulla soluta saepe ab numquam dolorum recusandae ducimus
-          qui aspernatur, sapiente commodi explicabo! Cupiditate deserunt
-          quaerat provident esse ad ab accusantium dolores quia illo!
-        </p>
-        <WebPageLink :link="'#'" :link_text="'work.com'" :type="'website'" />
+  <div class="work-wrapper">
+    <div class="work-block">
+      <div class="work-img">
+        <img :src="imageUrl" alt="Work Image" />
       </div>
-      <ButtonRedirect :btn_text="'About it'" :target_page="'/works/1#'" />
+      <div class="work-data-block">
+        <div class="work-text-block">
+          <h3 class="work-title">{{ work.work_name }}</h3>
+          <p class="about-work-text">
+            {{ work.work_details }}
+          </p>
+          <WebPageLink
+            :link="work.link"
+            :link_text="linkText"
+            :type="'website'"
+          />
+        </div>
+        <ButtonRedirect
+          :btn_text="'About it'"
+          :target_page="`/works/${work.work_id}`"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -31,6 +31,7 @@ import WebPageLink from "@/components/WebPageLink.vue";
 
 export default {
   name: "WorkItem",
+  props: ["work"],
   data() {
     return {};
   },
@@ -38,40 +39,100 @@ export default {
     ButtonRedirect,
     WebPageLink,
   },
+  computed: {
+    linkText() {
+      return this.work.link.split("://")[1];
+    },
+    imageUrl() {
+      return `${this.work.cover_image_type}, ${this.work.cover_image}`;
+    },
+  },
 };
 </script>
 
-<style lang="scss" scoped>
-.work-block {
+<style lang="scss">
+.work-wrapper {
   border-bottom: 1px solid var(--additional-dark-mode-color);
   padding-bottom: 20px;
-  display: flex;
-  max-height: 260px;
-  .work-img {
-    margin-right: 40px;
-    img {
-      width: 480px;
+  .work-block {
+    display: flex;
+    width: 480px;
+    .work-img {
+      margin-right: 40px;
+      width: inherit;
       height: 240px;
+      img {
+        width: inherit;
+        height: inherit;
+      }
+    }
+    .work-data-block {
+      .work-text-block {
+        .work-title {
+          margin-bottom: 15px;
+        }
+        .about-work-text {
+          margin-bottom: 10px;
+          font-size: 0.9em;
+          overflow: hidden;
+          line-height: 1.1em;
+          height: 5.3em;
+          display: -webkit-box;
+          -webkit-box-orient: vertical;
+          -webkit-line-clamp: 5;
+        }
+      }
+      .btn-redirect {
+        margin-top: 30px;
+      }
     }
   }
-  .work-data-block {
-    .work-text-block {
-      .work-title {
-        margin-bottom: 15px;
+}
+
+@media (max-width: 870px) {
+  .work-wrapper {
+    display: flex;
+    justify-content: center;
+    .work-block {
+      display: block;
+      max-height: none;
+      .work-img {
+        margin-right: 0;
       }
-      .about-work-text {
-        margin-bottom: 10px;
-        font-size: 0.9em;
-        overflow: hidden;
-        line-height: 1.1em;
-        height: 5.3em;
-        display: -webkit-box;
-        -webkit-box-orient: vertical;
-        -webkit-line-clamp: 5;
+      .work-data-block {
+        padding: 10px 20px;
+        height: 240px;
       }
     }
-    .btn-redirect {
-      margin-top: 30px;
+  }
+}
+
+@media (max-width: 630px) {
+  .work-wrapper {
+    .work-block {
+      width: 360px;
+      .work-img {
+        width: inherit;
+        height: 180px;
+      }
+      .work-data-block {
+        width: inherit;
+      }
+    }
+  }
+}
+
+@media (max-width: 430px) {
+  .work-wrapper {
+    .work-block {
+      width: 240px;
+      .work-img {
+        width: inherit;
+        height: 120px;
+      }
+      .work-data-block {
+        width: inherit;
+      }
     }
   }
 }
