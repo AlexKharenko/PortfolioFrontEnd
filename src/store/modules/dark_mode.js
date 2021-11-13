668711511daa1f413ca8101aa0121ddb57d8ca7d
AlexKharenko <alexey16052002@gmail.com>
2021-11-13 23:14:16 +0200

    Dark mode saving in local storage

--- src/store/modules/dark_mode.js
+++ src/store/modules/dark_mode.js
@@ -1,5 +1,5 @@
 const state = {
-  dark_mode_on: false,
+  dark_mode_on: JSON.parse(localStorage.getItem("dark_mode")) || false,
 };
 
 const getters = {
@@ -8,6 +8,7 @@ const getters = {
 
 const actions = {
   changeDarkModeActivity({ commit }, status) {
+    localStorage.setItem("dark_mode", JSON.stringify(status));
     commit("setDarkModeActivity", status);
   },
 };
