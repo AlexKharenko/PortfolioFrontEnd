17ef7496e6305f9347e774bb8dc5e4f21db9b6ab
AlexKharenko <alexey16052002@gmail.com>
2021-11-14 19:17:18 +0200

    Add about as multilang

---
+++ src/components/Skills.vue
@@ -0,0 +1,105 @@
+<template>
+  <div class="skills-wrapper">
+    <h3>{{ $t("about.skills") }}:</h3>
+    <div class="skills-icons-block">
+      <div class="img-container">
+        <img src="../assets/skills_icons/vue.png" alt="Vue icon" />
+      </div>
+      <div class="img-container">
+        <img
+          class="filter"
+          src="../assets/skills_icons/html.png"
+          alt="HTML5 icon"
+        />
+      </div>
+      <div class="img-container">
+        <img
+          class="filter"
+          src="../assets/skills_icons/css-3.png"
+          alt="Css3 icon"
+        />
+      </div>
+      <div class="img-container">
+        <img
+          class="filter"
+          src="../assets/skills_icons/js.png"
+          alt="JavaScript icon"
+        />
+      </div>
+      <div class="img-container">
+        <img src="../assets/skills_icons/node-js.png" alt="Node.js icon" />
+      </div>
+      <div class="img-container">
+        <img
+          class="filter"
+          src="../assets/skills_icons/postgre.png"
+          alt="PostgreSQL icon"
+        />
+      </div>
+      <div class="img-container">
+        <img
+          class="filter"
+          src="../assets/skills_icons/python.png"
+          alt="Python icon"
+        />
+      </div>
+      <div class="img-container">
+        <img
+          class="filter"
+          src="../assets/skills_icons/git.png"
+          alt="Git icon"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Skills",
+};
+</script>
+
+<style lang="scss">
+.skills-wrapper {
+  .skills-icons-block {
+    display: grid;
+    grid-template-columns: repeat(4, 120px);
+    grid-gap: 20px;
+    justify-content: center;
+    .img-container {
+      img {
+        transition: 0.4s ease-in-out;
+        height: auto;
+        width: 100%;
+      }
+    }
+  }
+}
+.dark {
+  .skills-wrapper {
+    .skills-icons-block {
+      .img-container {
+        img.filter {
+          filter: invert(1);
+        }
+      }
+    }
+  }
+}
+
+@media (max-width: 610px) {
+  .skills-wrapper {
+    .skills-icons-block {
+      grid-template-columns: repeat(4, 80px);
+    }
+  }
+}
+@media (max-width: 430px) {
+  .skills-wrapper {
+    .skills-icons-block {
+      grid-template-columns: repeat(2, 80px);
+    }
+  }
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -6,14 +6,56 @@
     </div>
     <div class="about-me-text-block">
       <p class="about-me-paragraph">
-        Something about me. Lorem ipsum dolor sit amet consectetur adipisicing
-        elit. Nulla quasi eius dicta, aspernatur ipsam assumenda, magnam,
-        debitis esse nobis nisi nesciunt quas enim. Enim vitae nemo minus!
-        Omnis, dolorem laboriosam!
+        {{ $t("about.main_message") }}
       </p>
+      <Skills />
+      <div class="list-wrapper">
+        <h3>{{ $t("about.education.title") }}:</h3>
+        <div class="list-block">
+          <div
+            class="list"
+            v-for="(education, i) in $i18n.messages[$i18n.locale].about
+              .education.education_array"
+            :key="i"
+          >
+            <p class="education-gen">
+              <span class="university-title">{{ education.university }}</span>
+              <span class="city"> ,{{ education.city }}</span>
+              <span class="degree"> - {{ education.degree }}</span>
+            </p>
+            <p class="period">{{ education.period }}</p>
+          </div>
+        </div>
+      </div>
+      <div class="list-wrapper">
+        <h3>{{ $t("about.languages.title") }}:</h3>
+        <div class="list-block">
+          <div
+            class="list"
+            v-for="(language, i) in $i18n.messages[$i18n.locale].about.languages
+              .languages_array"
+            :key="i"
+          >
+            <p class="language">
+              {{ language.lang }} - {{ language.lang_level }}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
+<script>
+import Skills from "@/components/Skills.vue";
+
+export default {
+  name: "About",
+  components: {
+    Skills,
+  },
+};
+</script>
 
 <style lang="scss">
 .about {
@@ -23,8 +65,32 @@
     margin-bottom: 30px;
   }
   .about-me-text-block {
+    h3 {
+      margin: 30px 20px;
+    }
     .about-me-paragraph {
       text-indent: 45px;
+    }
+    .list-wrapper {
+      .list-block {
+        .list {
+          .education-gen {
+            .university-title {
+              font-weight: 600;
+            }
+            .degree {
+              font-style: italic;
+            }
+          }
+          .period {
+            margin-top: 10px;
+            color: var(--additional-dark-mode-color);
+          }
+          .language {
+            margin-top: 5px;
+          }
+        }
+      }
     }
   }
 }
